3d6fee494c03a853be3f1410b96cdb913d265e58
virtuops <support@virtuops.com>
2018-05-14 08:54:55 -0400

    Added tasks to SQL

--- install/updateactions.sh
+++ install/updateactions.sh
@@ -6,7 +6,7 @@
 # Property of MKAdvantage, Inc, All Rights Reserved 2017
 #
 
-basedir=`cat /var/www/html/nochero/app/server/config.ini | grep basedir | cut -f2 -d= | tr -d '"' | tr -d '[:space:]'`
+basedir=`cat /var/www/html/pilot/app/server/config.ini | grep basedir | cut -f2 -d= | tr -d '"' | tr -d '[:space:]'`
 
 
 alltasks=`/usr/bin/mysql -unhp123!!! -p'nhp123!!!' -A nochero_pilot -e 'select actionfilename, actiontext from tasks \G'`
